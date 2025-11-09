7fb2f8eea9b15ae3ace28808507861ecfcfa2dce
Alex Walker <aiw2005@icloud.com>
2025-11-09 03:40:50 +0000

    Object creation setup for obstacle+oxygen
    Code to create objects + scenes made. Does not handle generation yet

--- spooky-jam-test-project/player.gd
+++ spooky-jam-test-project/player.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+signal itemCollide
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -8,3 +9,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if !global.gameOver:
 		position.x += (global.speed * delta)
+
+func _on_body_entered(body: Node2D) -> void:
+	body.set_deferred("CollisionShape2D", false)
+	if body.get_groups().has("Collectable"):
+		global.oxygen = min(global.oxygen + body.get_meta("OxygenGiven"), 100)
+	if body.get_groups().has("obstacle"):
+		global.oxygen = max(global.oxygen - body.get_meta("OxygenLost"), 0)
+	body.queue_free()
